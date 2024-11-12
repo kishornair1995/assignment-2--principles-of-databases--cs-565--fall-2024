0454085f50039472f336072b187f09f6a4f4c463
kishornair1995 <kishornair1995@gmail.com>
2024-11-12 00:22:59 -0500

    configured to connect the database

--- includes/functions.php
+++ includes/functions.php
@@ -24,8 +24,6 @@ function getComputerInventory($pdo) {
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-
-
 function getOSSupport($pdo) {
     $stmt = $pdo->prepare("SELECT model, installed_os, last_supported_os FROM os_support ORDER BY model ASC");
     $stmt->execute();
